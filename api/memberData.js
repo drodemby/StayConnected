c758173f4ea8012ef30cf9af2ac2493d900892eb
Dro Demby <drotechdemby@gmail.com>
2023-02-28 21:03:48 -0600

    Created and added api keys to data files in the api folder

---
+++ api/memberData.js
@@ -0,0 +1,70 @@
+import { clientCredentials } from '../utils/client';
+
+const dbUrl = clientCredentials.databaseURL;
+
+const getMembers = () => new Promise ((resolve, reject) => {
+
+  fetch(`${dbUrl}/member.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const newMember = (payload) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/member.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateMember = (payload) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/member/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const deleteMember = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/member/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  .then((response) => response.json())
+  .then((data) => resolve(data))
+  .catch(reject);
+});
+
+
+export {
+
+  getMembers,
+  newMember,
+  deleteMember,
+  updateMember,
+  
+};
---
+++ api/mergedData.js
@@ -0,0 +1,30 @@
+import { memo } from 'react';
+import { deleteSingleMinistry, getMembersByMinistry, getSingleMinistry } from './ministryData';
+import { deleteSingleMember, getSingleMember } from './memberData';
+
+const viewMinistryDetails = (firebaseKey) => new Promise((resolve, reject) => {
+  getSingleMinistry(firebaseKey).then((ministry) => {
+    getMembersByMinistry(ministry.firebaseKey)
+      .then((ministryMembers) => resolve({ ...ministry, ministryMembers }));
+  }).catch(reject);
+});
+
+const viewMemberDetails = (firebaseKey) => new Promise((resolve, reject) => {
+  getSingleMember(firebaseKey).then((member) => {
+    getSingleMinistry(member.ministry_id)
+      .then((memberData) => resolve({ ...member, memberData }));
+  }).catch(reject);
+});
+
+const deleteMinistryMembers = (ministryId) => new Promise((resolve, reject) => {
+  getPinsByBoard(ministryId).then((memberArray) => {
+    // console.warn(meberArray, 'Board Pins');
+    const deleteMemberPromises = memberArray.map((member) => deleteSingleMember(member.firebaseKey));
+
+    Promise.all(deleteMemberPromises).then(() => {
+      deleteSingleMinistry(ministryId).then(resolve);
+    });
+  }).catch((error) => reject(error));
+});
+
+export { viewMinistryDetails, deleteMinistryMembers, viewMemberDetails };
