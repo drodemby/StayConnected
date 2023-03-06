f55bd5da263d60d2fbf1f6536f99a66d6d0ded01
Dro Demby <drotechdemby@gmail.com>
2023-03-06 04:34:58 -0600

    Added suggestion form to my components

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,43 +1,50 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-// import Link from 'next/link';
-import Image from 'next/image';
-// import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-// import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import SearchBar from './SearchBar';
-import { useAuth } from '../utils/context/authContext';
+import Link from 'next/link';
+import { signOut } from '../utils/auth';
 
 export default function NavBar() {
-  const { user } = useAuth();
-
   return (
-    <Navbar expand="lg" id="navbar">
-      <Container fluid>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="/">Home</Nav.Link>
-            <NavDropdown title="Create" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="/pin/new">Create Pin</NavDropdown.Item>
-              <NavDropdown.Item href="/board/new">
-                Create Board
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <SearchBar className="d-flex" />
-          <Nav.Link href="/profile">
-            <Image src={user.photoURL} alt="userURL" width="50px" height="50px" id="navbarprofile" />
-          </Nav.Link>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link passHref href="/">
+          <a className="navbar-brand" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01">
+            CHANGE ME
+          </a>
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <Link passHref href="/">
+                <a className="nav-link">
+                  Home
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link passHref href="/">
+                <a className="nav-link">
+                  Home
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link passHref href="/">
+                <a className="nav-link">
+                  Home
+                </a>
+              </Link>
+            </li>
+            <button type="button" className="btn btn-danger" onClick={signOut}>
+              Sign Out
+            </button>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
