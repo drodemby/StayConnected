bc2076e3615cd0954d192184035d6efeede24b4b
Dro Demby <drotechdemby@gmail.com>
2023-04-08 07:59:31 -0500

    Changed homepage image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
-import cotwgi from '../images/cotwgi.png';
+// eslint-disable-next-line import/no-unresolved
+import stockPic from '../images/stockPic.png';
 
 function Home() {
   return (
@@ -9,7 +10,7 @@ function Home() {
       <h1>Welcome to StayConnected</h1>
       <p>We are so glad that you have found us and hope that our online presence can provide you with a glimpse of who we are and what we stand for. Our church community is open and welcoming to everyone, regardless of your background, beliefs, or current life circumstances. <br /> Our mission is to spread love, kindness, and compassion in the world, and we believe that starts with each and every person who walks through our doors or visits us online. We offer a variety of services, programs, and events that are designed to help you grow spiritually, connect with others, and serve our community. So take a look around, get to know us better, and feel free to reach out if you have any questions or if there is anything we can do to support you on your spiritual journey. Thank you for visiting our site, and we hope to see you soon!</p>
 
-      <Image src={cotwgi} alt="Church pic" />
+      <Image src={stockPic} alt="Church pic" />
     </div>
   );
 }
